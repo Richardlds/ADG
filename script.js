6d36ed6aed8ab06fa5def63946d41a9110aedf17
Richardlds <117732146+Richardlds@users.noreply.github.com>
2023-06-26 12:53:24 -0300

    1

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-const images = document.querySelectorAll('.banner img');
-let currentImageIndex = 0;
-images[currentImageIndex].classList.add('active');
-
-function changeImage() {
-  images[currentImageIndex].classList.remove('active');
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  images[currentImageIndex].classList.add('active');
-}
-
-setInterval(changeImage, 5000);
-function showPreviousImage() {
-  let newIndex = (currentImageIndex - 1 + images.length) % images.length;
-  changeImage(newIndex);
-}
-
-function showNextImage() {
-  let newIndex = (currentImageIndex + 1) % images.length;
-  changeImage(newIndex);
-}
-
-document.querySelector('.prev-button').addEventListener('click', showPreviousImage);
-document.querySelector('.next-button').addEventListener('click', showNextImage);
-
-
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const dropdownMenu = document.querySelector('.dropdown-menu');
 
@@ -37,3 +12,28 @@ document.addEventListener('click', (event) => {
   }
 });
 
+const images = document.querySelectorAll('.banner img');
+let currentImageIndex = 0;
+images[currentImageIndex].classList.add('active');
+
+function changeImage() {
+  images[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].classList.add('active');
+}
+
+setInterval(changeImage, 5000);
+
+function showPreviousImage() {
+  let newIndex = (currentImageIndex - 1 + images.length) % images.length;
+  changeImage(newIndex);
+}
+
+function showNextImage() {
+  let newIndex = (currentImageIndex + 1) % images.length;
+  changeImage(newIndex);
+}
+
+document.querySelector('.prev-button').addEventListener('click', showPreviousImage);
+document.querySelector('.next-button').addEventListener('click', showNextImage);
+  
